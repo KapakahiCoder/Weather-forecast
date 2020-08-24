a273200b1e7ed1a4cb3d358e2ae8bdc68a17823f
Kapakahi <rubbishman@gmail.com>
2020-08-24 14:16:20 +0900

    changing structure of app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,74 @@
 <template>
   <div id="app">
-    <GetLocation />
+    <h1>Let's checkout the weather forecast for your area</h1>
+    <form @submit.prevent>
+      <label>Search area: </label>
+      <input type="text" v-model="code" />
+      <p> 
+        <button @click="search(code)">Search</button> 
+      </p>
+    </form>
+    <div>
+      <h3>Here is code: {{code}} </h3>
+      <h3>Please enter valid Japanese postal code without any hypens or spaces</h3>
+    </div>
+    <div v-if = "responseAvailable == true"> 
+        <hr>
+          <h2>Here is the weather report for {{state}}, {{town}}: </h2>
+        <hr> 
+    </div>
     <Weather />
     <Map />
   </div>
 </template>
 
 <script>
-import GetLocation from './components/GetLocation.vue'
 import Weather from './components/Weather.vue'
 import Map from './components/Map.vue'
 
 export default {
+  data() {
+    return {
+      code: null,
+      responseAvailable: false,
+      state: null,
+      town: null,
+    }
+  },
   name: 'App',
   components: {
-    GetLocation,
     Weather,
     Map
+  },
+  methods: {
+    search: function(postalCode) {
+  this.responseAvailable = false;    
+      fetch('https://postcodejp-api.p.rapidapi.com/postcodes?postcode=' + postalCode, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "postcodejp-api.p.rapidapi.com",
+		"x-rapidapi-key": "43aab3d406mshab354846eb51230p1e10f3jsn1f88db389414"
+	}
+})
+.then(response => {
+    if (response) {
+      return response.json()
+    } else {
+      alert ("Server returned " + response.status + " : " + response.statusText );
+    }
+})
+.then(response => {
+  console.log("location object: ", response.data[0]);
+  this.state = response.data[0].state;
+  this.town = response.data[0].town;
+  this.responseAvailable = true;
+  console.log("town: ", this.town);
+  console.log("state: ", this.state);
+})
+.catch(err => {
+	console.log(err);
+});
+    }
   }
 }
 </script>
