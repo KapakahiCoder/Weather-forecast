1137e00b6c74848d9e761446441582ab849fe585
Kapakahi <rubbishman@gmail.com>
2020-08-24 12:22:59 +0900

    Map component can get long and lat successfully

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,11 @@
 <template>
-  <h2>MAP</h2>
+  <div>
+    <h2>MAP</h2>
+    <h3>  {{town}}   </h3>
+     <p> 
+        <button @click="getCoords">Search</button> 
+      </p>
+  </div>
 </template>
 
 
@@ -7,10 +13,41 @@
 export default {
   data() {
     return {
-   
+      long: null,
+      lat: null
     }
   },
-  name: 'Map'
+  name: 'Map',
+  props: ['town'],
+  methods: {
+    getCoords: function() {
+      fetch('https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&address=2720022', {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
+		"x-rapidapi-key": "43aab3d406mshab354846eb51230p1e10f3jsn1f88db389414"
+	}
+})
+.then(response => {
+  console.log("first" , response);
+  if (response) {
+    return response.json()
+  } else {
+    alert ("Server returned " + response.status + " : " + response.statusText );
+  }
+})
+.then(response => {
+  console.log("final: ", response)
+  this.long = response.results[0].geometry.location.lng;
+  this.lat = response.results[0].geometry.location.lat
+  console.log("long: ", this.long);
+  console.log("lat: ", this.lat);
+})
+.catch(err => {
+	console.log(err);
+});
+    }
+  }
   
 }
 </script>
